4ed3a45b009cf313ad143e2b3a721da9fe9c7f5e
David Rodal <dave.rodal@gmail.com>
2013-06-04 20:39:59 -0400

    removed restart game from menu item.

--- TMCW/view.php
+++ TMCW/view.php
@@ -18,7 +18,7 @@
                         <li><a id="muteButton">mute</a></li>
                         <li><a href="<?=site_url("wargame/leaveGame");?>">Go To Lobby</a></li>
                         <li><a href="<?=site_url("wargame/logout");?>">logout</a></li>
-                        <li><a href="<?=site_url("wargame/unitInit/MartianCivilWar");?>">Restart Game</a></li>
+<!--                        <li><a href="--><?//=site_url("wargame/unitInit/MartianCivilWar");?><!--">Restart Game</a></li>-->
                         <li><a href="#" onclick="seeUnits();return false;">See Units</a></li>
                         <li><a href="#" onclick="seeBoth();return false;">See Both</a></li>
                         <li><a href="#" onclick="seeMap();return false;">See Map</a></li>
