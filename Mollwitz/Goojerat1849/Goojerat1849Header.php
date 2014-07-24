7f48042dfe0a8c2350b88a1c82d04254a7342abd
daverodal <dave.rodal@gmail.com>
2014-07-23 22:05:54 -0400

    Goojerat1849 game ready for terrain map

---
+++ Mollwitz/Goojerat1849/Goojerat1849Header.php
@@ -0,0 +1,3 @@
+<style type="text/css">
+<?php include "all.css";?>
+</style>
---
+++ Mollwitz/Goojerat1849/victoryConditions.php
@@ -0,0 +1,14 @@
+<li><span class="lessBig">Victory Conditions</span>
+    <ol>
+        <li>12 Turns</li>
+        <li>British win at 45 points with a lead of 15</li>
+        <li>All Sikh losses are scored a face value</li>
+        <li>Sikhs win if they score 30 points or British don't win by turn 12.</li>
+        <li>All Royal units are scored at double value including Inf.</li>
+        <li>All Native units are scored at face value.</li>
+        <li>There are points values on some hexes Red for British, black for Sikh</li>
+        <li>All artillery is double defense in clear</li>
+        <li>Sikh Infantry +1 combat point in Jungle/Scrub and Town</li>
+        <li>Sikhs do not get combined arms bonus with cavalry.</li>
+    </ol>
+</li>
