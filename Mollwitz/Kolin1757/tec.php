b059298052555a405adf7d39521e3da58476a4eb
daverodal <dave.rodal@gmail.com>
2014-08-09 14:42:54 -0400

    kolin tec update

--- Mollwitz/Kolin1757/tec.php
+++ Mollwitz/Kolin1757/tec.php
@@ -1,6 +1,6 @@
 <div class="dropDown"  id="TECWrapper">
     <h4 class="WrapperLabel" title='Terrain Effects Chart'>TEC</h4>
     <DIV id="TEC" style="display:none;"><div class="close">X</div>
-        <img id="tecImage" src="<?=base_url()?>js/ScimatarsTec1.png">
+        <img id="tecImage" src="<?=base_url()?>js/KolinTEC.jpg">
     </div>
 </div>
