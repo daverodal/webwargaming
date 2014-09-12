3e15ead6ed3e4d4e65b34dd71af107d3355bd362
david_rodal <dave.rodal@gmail.com>
2014-09-12 12:49:32 -0400

    Fixed retreat check for firstHex and zoc

--- moveRules.php
+++ moveRules.php
@@ -538,7 +538,7 @@ class MoveRules
             if ($this->moves->$hexNum->isZoc == NULL) {
                 $this->moves->$hexNum->isZoc = $this->force->mapHexIsZOC($mapHex, $defendingForceId);
             }
-            if ((!$hexPath->firstHex) && $this->moves->$hexNum->isZoc) {
+            if ($this->moves->$hexNum->isZoc) {
                 continue;
             }
             $path = $hexPath->pathToHere;
