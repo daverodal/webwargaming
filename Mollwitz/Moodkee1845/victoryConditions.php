2189ba78d0f2c97ffe5ab517a9bee9d90cf85851
daverodal <dave.rodal@gmail.com>
2014-11-13 23:46:52 -0500

    Victory Conditions typo

--- Mollwitz/Moodkee1845/victoryConditions.php
+++ Mollwitz/Moodkee1845/victoryConditions.php
@@ -4,7 +4,7 @@
         <li>British win at 45 points with a lead of 15</li>
         <li>All Sikh losses are scored a face value</li>
         <li>Sikhs win if they score 30 points</li>
-        <li>If both players win at end of the game, or neither player win at end of game, the results in a tie.</li>
+        <li>If both players win at end of the game, or neither player wins at end of game, the results in a tie.</li>
         <li>All Royal units are scored at double value including Inf.</li>
         <li>All Native units are scored at face value.</li>
         <li>There are points values on some hexes Red for British, black for Sikh</li>
