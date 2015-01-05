079cc74f4af0e582dac94197ebfdc4e1d8548fa3
david_rodal <dave.rodal@gmail.com>
2015-01-05 14:01:40 -0500

    Rules changes for modern games

--- TMCW/commonMoveRules.php
+++ TMCW/commonMoveRules.php
@@ -2,11 +2,11 @@
 
 <p>The Second Number on the counter is Movement Points <abbr title="Movement Points">(MP)</abbr>.</p>
 
-<h4>Units expend different amounts of <abbr title="Movement Points">MP</abbr> for different terrains.
+<p>Units expend different amounts of <abbr title="Movement Points">MP</abbr> for different terrains.
     Also,
     different units may expend
-    different amounts to enter certain hexes.</h4>
-<ul>
+    different amounts to enter certain hexes.</p>
+<ol>
     <li>The number of movement points or <abbr title="Movement Points">MP</abbr>'s a unit expends to
         enter a
         hex is baseed
@@ -17,14 +17,13 @@
             title="Terrain Effects Chart">TEC</abbr> may be found by pressing
         the button <abbr title="Terrain Effects Chart">TEC</abbr>.
     </li>
-</ul>
-<h3>Road Movement</h3>
-<ul>
 
-    <li><p>When a unit moves from a hex containing a road, to another hex containing a road, and a road
+
+    <li>Road Movement
+<p>        When a unit moves from a hex containing a road, to another hex containing a road, and a road
             traverses the hexside
             be traversed, the unit may be eligible for road movement. Road movement often requires less
             <abbr title="Movement Points">MP</abbr>'s than the other terrain in the hex.</p>
 
     </li>
-</ul>
+</ol>
