32689b9cbf28c5392919e96d7760ff7e8e8a904f
daverodal <dave.rodal@gmail.com>
2015-02-14 15:25:59 -0500

    HQ MP fix

--- Mollwitz/Goojerat1849/Goojerat1849.php
+++ Mollwitz/Goojerat1849/Goojerat1849.php
@@ -79,7 +79,7 @@ class Goojerat1849 extends IndiaCore
                 $this->force->addUnit("infantry-1", SIKH_FORCE, "deployBox", "SikhInfBadge.png", 1, 1, 5, true, STATUS_CAN_DEPLOY, "A", 1, 1, "Sikh", false, 'hq');
             }
             for ($i = 0; $i < 4; $i++) {
-                $this->force->addUnit("infantry-1", BRITISH_FORCE, "deployBox", "BritInfBadge.png", 7, 7, 4, true, STATUS_CAN_DEPLOY, "B", 1, 1, "British", false, 'hq');
+                $this->force->addUnit("infantry-1", BRITISH_FORCE, "deployBox", "BritInfBadge.png", 1, 1, 5, true, STATUS_CAN_DEPLOY, "B", 1, 1, "British", false, 'hq');
             }
         }
         if ($scenario->theMoodkeeOOB) {
