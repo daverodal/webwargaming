5381d972c3287bd45d83d96aee7a537dcb6f2cef
daverodal <dave.rodal@gmail.com>
2015-04-12 13:21:20 -0400

    New TEC for all new games

--- Mollwitz/Klissow1702/tec.php
+++ Mollwitz/Klissow1702/tec.php
@@ -18,6 +18,6 @@ You should have received a copy of the GNU General Public License
 ?><div class="dropDown"  id="TECWrapper">
     <h4 class="WrapperLabel" title='Terrain Effects Chart'>TEC</h4>
     <DIV id="TEC" style="display:none;"><div class="close">X</div>
-        <img id="tecImage" src="<?=base_url()?>js/KolinTEC.jpg">
+        <img id="tecImage" src="<?=base_url()?>js/OudenardeTECFinal.jpg">
     </div>
 </div>
