45c5667569108c6f1511a96e894e1a5c463f6114
daverodal <dave.rodal@gmail.com>
2015-05-28 22:09:23 -0400

    Allied instead of all 3 nations

--- Mollwitz/Hanau1813/Hanau1813.php
+++ Mollwitz/Hanau1813/Hanau1813.php
@@ -22,7 +22,7 @@ define("ALLIED_FORCE", 2);
 
 global $force_name;
 $force_name[FRENCH_FORCE] = "French";
-$force_name[ALLIED_FORCE] = "Austrian/Bavarian/Russian Allies";
+$force_name[ALLIED_FORCE] = "Allied";
 
 require_once "JagCore.php";
 
