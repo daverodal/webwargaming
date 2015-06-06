ced30a3cad226456fd6084018fde5944207d43ae
daverodal <dave.rodal@gmail.com>
2015-06-06 12:12:35 -0400

    moved transform code into here for fetchChanges
    Fixed klissow victory, added swedish pontoon
    
    changed some phpunit tests......... am I insane.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,6 +11,7 @@ set_include_path("Mollwitz". PATH_SEPARATOR .  get_include_path());
 set_include_path("Mollwitz/Klissow1702". PATH_SEPARATOR .  get_include_path());
 require_once("crtTraits.php");
 set_include_path("TMCW". PATH_SEPARATOR .  get_include_path());
+
 set_include_path("TMCW/Kiev". PATH_SEPARATOR .  get_include_path());
 
 //require_once "Mollwitz/Klissow1702/Klissow1702.php";
--- tests/displayTest.php
+++ tests/displayTest.php
@@ -3,7 +3,7 @@ class DisplayTest extends PHPUnit_Framework_TestCase
 {
 	public function testThis(){
 		$this->assertEquals(0,0);
-        $k = new Kiev();
+        $k = new Klissow1702();
 		$d = new Display();
         $v = new MapViewer();
 		$this->assertEquals(true, is_object($d));
