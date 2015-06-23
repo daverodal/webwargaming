9b2e89a745e921236c9f9b17603a010624e2ea0a
daverodal <dave.rodal@gmail.com>
2015-06-23 13:53:25 -0400

    Fontenoy 1745 i tell you want

---
+++ Mollwitz/Fontenoy1745/fontenoy1745VictoryCore.php
@@ -0,0 +1,168 @@
+<?php
+/*
+Copyright 2012-2015 David Rodal
+
+This program is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation;
+either version 2 of the License, or (at your option) any later version
+
+This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
+/**
+ * Created by JetBrains PhpStorm.
+ * User: markarianr
+ * Date: 5/7/13
+ * Time: 7:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+include "victoryCore.php";
+
+class fontenoy1745VictoryCore extends victoryCore
+{
+
+    function __construct($data)
+    {
+        if ($data) {
+            $this->movementCache = $data->victory->movementCache;
+            $this->victoryPoints = $data->victory->victoryPoints;
+            $this->gameOver = $data->victory->gameOver;
+            $this->deadGuardInf = $data->victory->deadGuardInf;
+        } else {
+            $this->victoryPoints = array(0, 0, 0);
+            $this->movementCache = new stdClass();
+            $this->gameOver = false;
+            $this->deadGuardInf = false;
+        }
+    }
+
+    public function save()
+    {
+        $ret = parent::save();
+        $ret->deadGuardInf = $this->deadGuardInf;
+        return $ret;
+    }
+    public function reduceUnit($args)
+    {
+        $unit = $args[0];
+        $mult = 1;
+        if($unit->nationality == "Guard"){
+            $mult = 1.5;
+            if($unit->class == "infantry" && $unit->maxStrength == 9){
+                $mult = 2.0;
+                $this->deadGuardInf = true;
+            }
+        }
+        $this->scoreKills($unit, $mult);
+    }
+
+    public function specialHexChange($args)
+    {
+        $battle = Battle::getBattle();
+
+        list($mapHexName, $forceId) = $args;
+        if (in_array($mapHexName, $battle->specialHexA)) {
+            if ($forceId == FRENCH_FORCE) {
+                $this->victoryPoints[FRENCH_FORCE]  += 5;
+                $battle->mapData->specialHexesVictory->$mapHexName = "<span class='french'>+5 French vp</span>";
+            }
+            if ($forceId == RUSSIAN_FORCE) {
+                $this->victoryPoints[RUSSIAN_FORCE]  -= 5;
+                $battle->mapData->specialHexesVictory->$mapHexName = "<span class='russian'>-5 French vp</span>";
+            }
+        }
+
+        if (in_array($mapHexName, $battle->specialHexB)) {
+            if ($forceId == FRENCH_FORCE) {
+                $this->victoryPoints[FRENCH_FORCE]  += 15;
+                $battle->mapData->specialHexesVictory->$mapHexName = "<span class='french'>+15 French vp</span>";
+            }
+            if ($forceId == RUSSIAN_FORCE) {
+                $this->victoryPoints[RUSSIAN_FORCE]  -= 15;
+                $battle->mapData->specialHexesVictory->$mapHexName = "<span class='russian'>-15 French vp</span>";
+            }
+        }
+    }
+
+    protected function checkVictory($attackingId, $battle)
+    {
+        $battle = Battle::getBattle();
+
+        $gameRules = $battle->gameRules;
+        $scenario = $battle->scenario;
+        $turn = $gameRules->turn;
+        $frenchWin = $RussianWin = $draw = false;
+
+        $victoryReason = "";
+
+        if (!$this->gameOver) {
+            $specialHexes = $battle->mapData->specialHexes;
+            $RussianWinScore = 30;
+            $frenchWinScore = 40;
+
+            if($this->victoryPoints[FRENCH_FORCE] >= $frenchWinScore){
+                $frenchWin = true;
+                $victoryReason .= "Over $frenchWinScore ";
+            }
+            if ($this->victoryPoints[RUSSIAN_FORCE] >= $RussianWinScore) {
+                $RussianWin = true;
+                $victoryReason .= "Over $RussianWinScore ";
+            }
+
+            if ($frenchWin && !$RussianWin) {
+                $this->winner = FRENCH_FORCE;
+                $gameRules->flashMessages[] = "French Win";
+                $gameRules->flashMessages[] = $victoryReason;
+                $gameRules->flashMessages[] = "Game Over";
+                $this->gameOver = true;
+                return true;
+            }
+            if ($RussianWin && !$frenchWin) {
+                $this->winner = RUSSIAN_FORCE;
+                $gameRules->flashMessages[] = "Allies Win";
+                $gameRules->flashMessages[] = $victoryReason;
+                $gameRules->flashMessages[] = "Game Over";
+                $this->gameOver = true;
+                return true;
+            }
+            if($frenchWin && $RussianWin){
+                $gameRules->flashMessages[] = "Tie Game";
+                $gameRules->flashMessages[] = $victoryReason;
+                $gameRules->flashMessages[] = "Game Over";
+                $this->gameOver = true;
+                return true;
+            }
+            if ($turn > $gameRules->maxTurn) {
+                $this->winner = RUSSIAN_FORCE;
+                $gameRules->flashMessages[] = "Allies Win";
+                $gameRules->flashMessages[] = "French Fail to Win";
+                $this->gameOver = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function postRecoverUnits($args)
+    {
+
+    }
+
+    public function postRecoverUnit($args)
+    {
+        $unit = $args[0];
+        $b = Battle::getBattle();
+        $scenario = $b->scenario;
+        $id = $unit->id;
+
+
+        parent::postRecoverUnit($args);
+
+    }
+}
---
+++ Mollwitz/Fontenoy1745/victoryConditions.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * Copyright 2012-2015 David Rodal
+ * User: David Markarian Rodal
+ * Date: 3/8/15
+ * Time: 5:48 PM
+ *
+ *  This program is free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation;
+ *  either version 2 of the License, or (at your option) any later version
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?><li class="exclusive">
+    <span class="lessBig">Victory Conditions</span>
+    <h4>The Game ends instantly at the end of any Player Turn when one or more of the following conditions applies. Mutual
+        victories are a Draw</h4>
+    <ol>
+        <li>Each side gains 1 victory point for each hostile combat factor destroyed.
+            Some hexes contain a blue number the French receive victory points = to the number
+            in each of those hexes when they occupy or where last to move through any of those hexes.
+        </li>
+
+
+        <li>French Win: If their total points reach 40 or more before the end of turn 9.</li>
+
+        <li>Russian Win: If their total points reach 30 or more or the French do not win by the end of turn 9.
+        </li>
+
+        <li>If at the end of any player turn both sides have somehow met their victory conditions above, the result is
+            a draw.
+        </li>
+    </ol>
+</li>
