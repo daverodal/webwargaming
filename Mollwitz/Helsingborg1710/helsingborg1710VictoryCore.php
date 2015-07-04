9d97fcc87f85c5abdb179ada9062c829c6c95f79
daverodal <dave.rodal@gmail.com>
2015-07-04 11:51:22 -0400

    Helsingborg1710 game pushed

---
+++ Mollwitz/Helsingborg1710/helsingborg1710VictoryCore.php
@@ -0,0 +1,173 @@
+<?php
+/*
+Copyright 2012-2015 David Rodal
+
+This program is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation;
+either version 2 of the License, or (at your option) any later version
+
+This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
+/**
+ * Created by JetBrains PhpStorm.
+ * User: markarianr
+ * Date: 5/7/13
+ * Time: 7:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+include "victoryCore.php";
+
+class helsingborg1710VictoryCore extends victoryCore
+{
+
+    function __construct($data)
+    {
+        if ($data) {
+            $this->movementCache = $data->victory->movementCache;
+            $this->victoryPoints = $data->victory->victoryPoints;
+            $this->gameOver = $data->victory->gameOver;
+            $this->deadGuardInf = $data->victory->deadGuardInf;
+        } else {
+            $this->victoryPoints = array(0, 0, 0);
+            $this->movementCache = new stdClass();
+            $this->gameOver = false;
+            $this->deadGuardInf = false;
+        }
+    }
+
+    public function save()
+    {
+        $ret = parent::save();
+        $ret->deadGuardInf = $this->deadGuardInf;
+        return $ret;
+    }
+    public function reduceUnit($args)
+    {
+        $unit = $args[0];
+        $mult = 1;
+        if($unit->nationality == "Guard"){
+            $mult = 1.5;
+            if($unit->class == "infantry" && $unit->maxStrength == 9){
+                $mult = 2.0;
+                $this->deadGuardInf = true;
+            }
+        }
+        $this->scoreKills($unit, $mult);
+    }
+
+    public function specialHexChange($args)
+    {
+        $battle = Battle::getBattle();
+
+        list($mapHexName, $forceId) = $args;
+        if (in_array($mapHexName, $battle->specialHexA)) {
+            if ($forceId == FRENCH_FORCE) {
+                $this->victoryPoints[FRENCH_FORCE]  += 10;
+                $battle->mapData->specialHexesVictory->$mapHexName = "<span class='french'>+10 French vp</span>";
+            }
+            if ($forceId == ALLIED_FORCE) {
+                $this->victoryPoints[FRENCH_FORCE]  -= 10;
+                $battle->mapData->specialHexesVictory->$mapHexName = "<span class='allies'>-10 French vp</span>";
+            }
+        }
+
+        if (in_array($mapHexName, $battle->specialHexB)) {
+            if ($forceId == ALLIED_FORCE) {
+                $this->victoryPoints[ALLIED_FORCE]  += 10;
+                $battle->mapData->specialHexesVictory->$mapHexName = "<span class='allies'>+10 Allied vp</span>";
+            }
+            if ($forceId == FRENCH_FORCE) {
+                $this->victoryPoints[ALLIED_FORCE]  -= 10;
+                $battle->mapData->specialHexesVictory->$mapHexName = "<span class='french'>-10 Allied vp</span>";
+            }
+        }
+    }
+
+    protected function checkVictory($attackingId, $battle)
+    {
+        $battle = Battle::getBattle();
+
+        $gameRules = $battle->gameRules;
+        $scenario = $battle->scenario;
+        $turn = $gameRules->turn;
+        $frenchWin = $alliesWin = $draw = false;
+
+        $victoryReason = "";
+
+        if (!$this->gameOver) {
+            $specialHexes = $battle->mapData->specialHexes;
+            $winScore = 50;
+
+            if($this->victoryPoints[FRENCH_FORCE] >= $winScore){
+                $frenchWin = true;
+                $victoryReason .= "Over $winScore ";
+            }
+            if ($this->victoryPoints[ALLIED_FORCE] >= $winScore) {
+                $alliesWin = true;
+                $victoryReason .= "Over $winScore ";
+            }
+
+            if ($frenchWin && !$alliesWin) {
+                $this->winner = FRENCH_FORCE;
+                $gameRules->flashMessages[] = "French Win";
+                $gameRules->flashMessages[] = $victoryReason;
+                $gameRules->flashMessages[] = "Game Over";
+                $this->gameOver = true;
+                return true;
+            }
+            if ($alliesWin && !$frenchWin) {
+                $this->winner = ALLIED_FORCE;
+                $gameRules->flashMessages[] = "Allies Win";
+                $gameRules->flashMessages[] = $victoryReason;
+                $gameRules->flashMessages[] = "Game Over";
+                $this->gameOver = true;
+                return true;
+            }
+            if($frenchWin && $alliesWin){
+                $gameRules->flashMessages[] = "Tie Game";
+                $gameRules->flashMessages[] = $victoryReason;
+                $gameRules->flashMessages[] = "Game Over";
+                $this->gameOver = true;
+                return true;
+            }
+            if ($turn > $gameRules->maxTurn) {
+                if($this->victoryPoints[FRENCH_FORCE] >= 25){
+                    $this->winner = ALLIED_FORCE;
+                    $gameRules->flashMessages[] = "French Win";
+                    $gameRules->flashMessages[] = "Allies Fail to Win";
+                }else{
+                    $gameRules->flashMessages[] = "Tie Game";
+                    $gameRules->flashMessages[] = "Game Over";
+                }
+
+                $this->gameOver = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function postRecoverUnits($args)
+    {
+
+    }
+
+    public function postRecoverUnit($args)
+    {
+        $unit = $args[0];
+        $b = Battle::getBattle();
+        $scenario = $b->scenario;
+        $id = $unit->id;
+
+
+        parent::postRecoverUnit($args);
+
+    }
+}
---
+++ Mollwitz/Helsingborg1710/victoryConditions.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ *
+ * Copyright 2012-2015 David Rodal
+ * User: David Markarian Rodal
+ * Date: 3/8/15
+ * Time: 5:48 PM
+ *
+ *  This program is free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation;
+ *  either version 2 of the License, or (at your option) any later version
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?><li class="exclusive">
+    <span class="lessBig">Victory Conditions</span>
+    <h4>The Game ends instantly at the end of any Player Turn when one or more of the following conditions applies. Mutual
+        victories are a Draw</h4>
+    <ol>
+        <li>Each side gains 1 victory point for each hostile combat factor destroyed.
+            Some hexes contain a blue number the French receive victory points = to the number
+            in each of those hexes when they occupy or where last to move through any of those hexes.
+        </li>
+
+        <li>French receive 10 points each for being the last to occupy or move through hexes marked with a Blue “10”.</li>
+
+        <li>Allies receive 10 points each for being the last to occupy or move through hexes marked with a Red “10”.</li>
+
+        <li>EITHER SIDE WINS: At the end of any turn that they have accumulated at least 50 points before the end of turn 10.</li>
+
+        <li>French Win: If the Allies have not won by the end of turn 10 and the French have at least 25 points.</li>
+
+        <li>If at the end of any player turn both sides have somehow met their victory conditions above, the result is
+            a draw.
+        </li>
+    </ol>
+</li>
