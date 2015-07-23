1816a2b21e259cd30dc358c558f505fefad5e167
david_rodal <dave.rodal@gmail.com>
2015-07-23 13:51:58 -0400

    Brandywine exclusive rules

--- Mollwitz/Brandywine1777/exclusiveRules.php
+++ Mollwitz/Brandywine1777/exclusiveRules.php
@@ -23,6 +23,7 @@ You should have received a copy of the GNU General Public License
  * Time: 12:21 PM added this
  * To change this template use File | Settings | File Templates.
  */
+
 ?>
 <style type="text/css">
 
@@ -55,27 +56,24 @@ You should have received a copy of the GNU General Public License
 
                     </ol>
                 </li>
-                <li><span class="lessBig">Terrain</span>
+                <li> <span class="lessBig">Demoralization</span>
                     <ol>
-                        <li>Swamps: The Swedes were able to get their guns over these swamps with little difficulty.
-                            In this game they will cause only  -1 MP to enter and -1 Combat factor to any unit attacking into or out of.</li>
                         <li>
-                            Sunken Road / Ravine. These were actually narrow ravines that these roads ran through in this game deduct 1 strength point from units attacking out.
-                            Artillery can only attack in or out to adjacent hexes.
-
+                            Once the Rebels have lost 30 combat strength points their units my not enter voluntarily enter hostile ZOC’s or attack.
                         </li>
                     </ol>
                 </li>
-                <li><span class="lessBig">Danish Indecision</span>
+                <li><span class="lessBig">Combat</span>
                     <ol>
-                        <li>Historically the Danes allowed the Swedes to advance over the swamps and on to their right flank with no intervention.</li>
-                        <li>
-                            At the beginning of the Danish movement phase turns 1-3 inclusive, the Danish player will roll 1d6.
-                            If the roll is 2 or less the Danish player may move no units that turn. They can stop rolling once the effect has occurred.
-                            If this effect does not happen by turn 4 the Danes may not move any units on turn 4. (the computer does this for you)
-
+                        <li><?= $playerOne?>: Units: Add 1 to their combat factor when defending in or attacking into clear terrain.
+                        </li>
+                        <li><?= $playerTwo?>: Units: Add 1 to their combat factor when defending in Hexes or all opponents are attacking across (hex sides) other than clear.
 
                         </li>
+                        <li>
+                            Attacks across Fords: The Hessians were able to force the fords with little difficulty. Do not half units attacking across the fords (Trail Bridges).
+                        </li>
+
                     </ol>
                 </li>
             </ol>
