6187ebe717ed2ff634dbd715cf7bbfa1fc205fd1
daverodal <dave.rodal@gmail.com>
2015-07-26 13:40:58 -0400

    helsingborg scenario fixes.

--- Mollwitz/Helsingborg1710/helsingborg1710VictoryCore.php
+++ Mollwitz/Helsingborg1710/helsingborg1710VictoryCore.php
@@ -171,25 +171,28 @@ class helsingborg1710VictoryCore extends victoryCore
         $id = $unit->id;
 
         parent::postRecoverUnit($args);
-        if ($b->gameRules->turn == 1 && $b->gameRules->phase == BLUE_MOVE_PHASE && $unit->status == STATUS_READY) {
-            $this->movementCache->$id = $unit->maxMove;
-            $unit->maxMove = $unit->maxMove+1;
-        }
-        if ($b->gameRules->turn == 1 && $b->gameRules->phase == BLUE_COMBAT_PHASE && isset($this->movementCache->$id)) {
-            $unit->maxMove = $this->movementCache->$id;
-            unset($this->movementCache->$id);
-        }
-        if ($b->gameRules->turn == 1 && $b->gameRules->phase == RED_MOVE_PHASE && $unit->status == STATUS_READY) {
-            $this->movementCache->$id = $unit->maxMove;
-            if($scenario->noMovementFirstTurn){
-                $unit->status = STATUS_UNAVAIL_THIS_PHASE;
-            }else{
-                $unit->maxMove = floor($unit->maxMove/2);
+        if(!$scenario->noSurprise) {
+            if ($b->gameRules->turn == 1 && $b->gameRules->phase == BLUE_MOVE_PHASE && $unit->status == STATUS_READY) {
+                $this->movementCache->$id = $unit->maxMove;
+                $unit->maxMove = $unit->maxMove + 1;
             }
-        }
-        if ($b->gameRules->turn == 1 && $b->gameRules->phase == RED_COMBAT_PHASE && isset($this->movementCache->$id)) {
-            $unit->maxMove = $this->movementCache->$id;
-            unset($this->movementCache->$id);
+            if ($b->gameRules->turn == 1 && $b->gameRules->phase == BLUE_COMBAT_PHASE && isset($this->movementCache->$id)) {
+                $unit->maxMove = $this->movementCache->$id;
+                unset($this->movementCache->$id);
+            }
+
+            if ($b->gameRules->turn == 1 && $b->gameRules->phase == RED_MOVE_PHASE && $unit->status == STATUS_READY) {
+                $this->movementCache->$id = $unit->maxMove;
+                if ($scenario->noMovementFirstTurn) {
+                    $unit->status = STATUS_UNAVAIL_THIS_PHASE;
+                } else {
+                    $unit->maxMove = floor($unit->maxMove / 2);
+                }
+            }
+            if ($b->gameRules->turn == 1 && $b->gameRules->phase == RED_COMBAT_PHASE && isset($this->movementCache->$id)) {
+                $unit->maxMove = $this->movementCache->$id;
+                unset($this->movementCache->$id);
+            }
         }
     }
 }
