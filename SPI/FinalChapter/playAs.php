a4f09628e779e9708275259969484c56c6e242ec
david_rodal <dave.rodal@gmail.com>
2015-07-31 13:44:45 -0400

    Final Chapter playas playmulti

--- SPI/FinalChapter/playAs.php
+++ SPI/FinalChapter/playAs.php
@@ -23,20 +23,22 @@
 <style>
     <?php @include "playAs.css";?>
     body{
-        background:url("<?=base_url("js/C-47_Dakotas_and_paratroops.jpg")?>") #333 no-repeat;
-        background-position:center 0;
+        background:url("<?=base_url("js/947px-United_States_bombing_raid_over_a_German_city_-_NARA_-_197269.jpg")?>") #333 no-repeat;
+        background-position:center center;
         background-size:100%;
     }
 </style>
 <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
 
 <h2 style="text-align:center;font-size:30px;font-family:'Great Vibes'"> Welcome to</h2>
-<h1 style="text-align:center;font-size:90px;font-family:'Lobster'">Uit de Hemel</h1>
+<h1 style="text-align:center;font-size:90px;font-family:'Lobster'">Final Chapter</h1>
 <div class="clear">&nbsp;</div>
 <fieldset ><Legend>Play As </Legend>
     <a class="link"  href="<?=site_url("wargame/enterHotseat");?>/<?=$wargame?>">Play Hotseat</a><br>
     <a class="link"  href="<?=site_url("wargame/enterMulti");?>/<?=$wargame?>">Play Multi Player </a><br>
     <a class="link" href="<?=site_url("wargame/leaveGame");?>">Go to Lobby</a>
     <div class="attribution">
-        By Sgt. D.M. Smith, British Army Film and Photographic Unit [Public domain], <a target="blank" href="http://commons.wikimedia.org/wiki/File%3AC-47_Dakotas_and_paratroops.jpg">via Wikimedia Commons</a>    </div>
+        By Unknown or not provided (U.S. National Archives and Records Administration) [Public domain], <a href="https://commons.wikimedia.org/wiki/File%3AUnited_States_bombing_raid_over_a_German_city_-_NARA_-_197269.jpg">via Wikimedia Commons</a>
+
+    </div>
 </fieldset>
--- SPI/FinalChapter/playMulti.php
+++ SPI/FinalChapter/playMulti.php
@@ -24,12 +24,12 @@
         body{
             background:#ccc;
             color:#333;
-            background: url("<?=base_url("js/1280px-Amphibious_Assault_Vehicle_(AAV).jpg")?>") #333 no-repeat;
-            background-position: 10% 0;
+            background: url("<?=base_url("js/947px-United_States_bombing_raid_over_a_German_city_-_NARA_-_197269.jpg")?>") #333 no-repeat;
+            background-position: center center;
             background-size:100%;
         }
 
     </style>
 <footer class="attribution">
-    By Mate 1st Class Brien Aho, U.S. Navy (http://www.navy.mil; exact Source) [Public domain], <a target="blank" href="http://commons.wikimedia.org/wiki/File%3AAmphibious_Assault_Vehicle_(AAV).jpg">via Wikimedia Commons</a>
+    By Unknown or not provided (U.S. National Archives and Records Administration) [Public domain], <a href="https://commons.wikimedia.org/wiki/File%3AUnited_States_bombing_raid_over_a_German_city_-_NARA_-_197269.jpg">via Wikimedia Commons</a>
 </footer>
