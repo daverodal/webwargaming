f6824b0ee1839857d2d733b754442787607c6b5a
daverodal <dave.rodal@gmail.com>
2015-09-06 14:00:36 -0400

    live fix movesLeft

--- NavalMoveRules.php
+++ NavalMoveRules.php
@@ -488,7 +488,7 @@ class NavalMoveRules
             if($unit->forceId !== $this->force->attackingForceId){
                 continue;
             }
-            if($unit->hits < 3 && $unit->maxMove > $unit->moveAmountUsed){
+            if($unit->hexagon->parent === "gameImages" && $unit->maxMove > $unit->moveAmountUsed){
                 return true;
             }
         }
