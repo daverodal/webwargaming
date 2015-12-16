0032e8e9fae90564e9536944545528f1382699a6
daverodal <dave.rodal@gmail.com>
2015-12-16 10:37:38 -0500

    rename lowercase file to uppsercase dance

--- TacticalCombatRules.php
+++ TacticalCombatRules.php
@@ -17,7 +17,7 @@
 //You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once "CombatRules.php";
+require_once "combatRules.php";
 class TacticalCombatRules extends CombatRules
 {
 
